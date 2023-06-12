fd194f224dfac44a0f048f47223f7fe497f4387b
shazzza88 <sharynwitt@gmail.com>
2023-06-12 14:37:17 +1200

    Add project files.

---
+++ 2018000415ass4/_2018000415ass4.py
@@ -0,0 +1,8 @@
+import pyodbc
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2018000415\OneDrive - Western Institute of Technology at Taranaki\python\ass4_database.accdb;')
+cursor = conn.cursor()
+cursor.execute('select * from tracking_sales')
+
+for row in cursor.fetchall():
+    print(row) 
+
