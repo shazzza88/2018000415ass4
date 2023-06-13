abcb0a8ac330ede8d98f0727bdfc839a2f20f8c0
shazzza88 <133725101+shazzza88@users.noreply.github.com>
2023-06-13 17:54:02 +1200

    Update _2018000415ass4.py
    changed spelling

--- 2018000415ass4/_2018000415ass4.py
+++ 2018000415ass4/_2018000415ass4.py
@@ -1,7 +1,7 @@
 import pyodbc
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2018000415\OneDrive - Western Institute of Technology at Taranaki\python\ass4_database.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from Customer_Table')
+cursor.execute('select * from Company_Table')
 
 for row in cursor.fetchall():
     print(row) 
