428a6b6f843edd8eaf3201db9e220b372b1a92a2
shazzza88 <sharynwitt@gmail.com>
2023-06-13 15:52:29 +1200

    query  datees between

--- 2018000415ass4/_2018000415ass4.py
+++ 2018000415ass4/_2018000415ass4.py
@@ -1,7 +1,7 @@
 import pyodbc
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2018000415\OneDrive - Western Institute of Technology at Taranaki\python\Python ass 4\Database4.accdb;')
 cursor = conn.cursor()
-cursor.execute( 'select * from Company_Table where Revenue_Growth > 0' )
+cursor.execute( 'select * from Company_Table WHERE Founded BETWEEN 01/01/1907 AND 01/01/1940;' )
 
 for row in cursor.fetchall():
     print(row) 
