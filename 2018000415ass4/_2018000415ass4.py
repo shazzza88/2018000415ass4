c672db364b26322b61dd58fe6e75367abfdaefb4
shazzza88 <sharynwitt@gmail.com>
2023-06-13 13:08:10 +1200

    Added Access file and selec * command

--- 2018000415ass4/_2018000415ass4.py
+++ 2018000415ass4/_2018000415ass4.py
@@ -1,7 +1,7 @@
 import pyodbc
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2018000415\OneDrive - Western Institute of Technology at Taranaki\python\ass4_database.accdb;')
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2018000415\OneDrive - Western Institute of Technology at Taranaki\python\Python ass 4\Database4.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from tracking_sales')
+cursor.execute( 'select * from Company_Table' )
 
 for row in cursor.fetchall():
     print(row) 
